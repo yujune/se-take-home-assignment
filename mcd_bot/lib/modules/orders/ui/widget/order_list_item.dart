1f8b25628786c8c2ef81837d7db1493ed93df19e
Tee Yu June <56582497+yujune@users.noreply.github.com>
2023-12-21 13:20:11 +0800

    feat: Add countdown display to orderlistitem.

--- mcd_bot/lib/modules/orders/ui/widget/order_list_item.dart
+++ mcd_bot/lib/modules/orders/ui/widget/order_list_item.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:lottie/lottie.dart';
@@ -6,34 +8,93 @@ import 'package:mcd_bot/modules/orders/ui/widget/oder_badge.dart';
 import 'package:mcd_bot/util/enum/local_date_format.dart';
 import 'package:mcd_bot/util/extension/context.dart';
 
-class OrderListItem extends StatelessWidget {
+class OrderListItem extends StatefulWidget {
   final Order order;
   const OrderListItem({super.key, required this.order});
+
+  @override
+  State<OrderListItem> createState() => _OrderListItemState();
+}
+
+class _OrderListItemState extends State<OrderListItem> {
+  Timer? _timer;
+
+  num _timeTaken = 0;
+
+  void setTimeTaken({required num value}) {
+    setState(() {
+      _timeTaken = value;
+    });
+  }
+
+  num calculateRemainingTime() {
+    final currentDateTime = DateTime.now();
+
+    final processingDateTime = widget.order.processingDate ?? DateTime.now();
+
+    final elapsedDateTime = currentDateTime.difference(processingDateTime);
+
+    return elapsedDateTime.inSeconds;
+  }
+
+  void checkAndStopTimer() {
+    if (_timer?.isActive == true) {
+      _timer?.cancel();
+      _timer = null;
+    }
+  }
+
+  void setupTimer() {
+    if (widget.order.status?.isProcessing != true) {
+      checkAndStopTimer();
+      return;
+    }
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      final value = calculateRemainingTime();
+      setTimeTaken(value: value);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _timeTaken = calculateRemainingTime();
+    setupTimer();
+  }
+
+  @override
+  void dispose() {
+    checkAndStopTimer();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: OrderBadge(
-        order: order,
+        order: widget.order,
       ),
       title: Text(
-        order.id ?? '-',
+        widget.order.id ?? '-',
         style: context.textTheme.bodySmall,
       ),
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "Status: ${order.status?.name}",
+            "Status: ${widget.order.status?.name}",
             style: context.textTheme.labelSmall,
           ),
           Text(
-            "Created Date: ${DateFormat(LocalDateFormat.dateWithTime.value).format(order.createdDate ?? DateTime.now())}",
+            "Created Date: ${DateFormat(LocalDateFormat.dateWithTime.value).format(widget.order.createdDate ?? DateTime.now())}",
             style: context.textTheme.labelSmall,
-          )
+          ),
+          if (widget.order.status?.isProcessing == true)
+            Text('Time taken: $_timeTaken'),
         ],
       ),
-      trailing: order.status?.isProcessing == true
+      trailing: widget.order.status?.isProcessing == true
           ? Lottie.asset("assets/lotties/cooking.json")
           : null,
     );
